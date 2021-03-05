e2aa5335a7a4dcf663d67d0b47f2b06ef08a3e10
Kevin Smith <ksmitw@gmail.com>
2021-03-04 21:45:04 -0500

    small changes to css and layout and a few adjustments to javascript I need to update later

--- src/main/resources/static/script/Testing.js
+++ src/main/resources/static/script/Testing.js
@@ -3,37 +3,41 @@ var client = null;
 
 
 
-function init(){
+function init() {
     let socket = new SockJS('/webSocket')
     client = Stomp.over(socket);
-    client.connect({},  (frame)=>{
+    client.connect({}, (frame) => {
         console.log('Connected');
         //This subscribes to our backend sendTo on the controller.
-        client.subscribe('/messsage',  (response)=>{
-            testMe(response);
+        client.subscribe('/messsage', (response) => {
+            updateText(response);
         })
     })
 
-//event listener adders 
-document.getElementById("fire").addEventListener("click", makeCall,false);
+    //event listener adders 
+    document.getElementById("fire").addEventListener("click", makeCall, false);
 
-function makeCall(e){
-    e.preventDefault();
-    client.send("/app/sendMessage",{}, JSON.stringify({'userName': document.getElementById('username').value,'message':document.getElementById('input').value}));
-    document.getElementById('input').value='';
+    function makeCall(e) {
+        e.preventDefault();
+        client.send("/app/sendMessage", {}, JSON.stringify({ 'userName': document.getElementById('username').value, 'message': document.getElementById('input').value }));
+        document.getElementById('input').value = '';
+    }
+
+    function updateText(response) {
+
+        let returnedResponse = JSON.parse(response.body);
+
+        if (returnedResponse.userName == document.getElementById('username').value) {
+            document.getElementById('responsArea').innerHTML += "<p class='current-user'>" + returnedResponse.message + "</p>";
+        }
+        else {
+            document.getElementById('responsArea').innerHTML += "<p>" + returnedResponse.userName + ": " + returnedResponse.message + "</p>";
+        }
+    }
 }
 
-function testMe(response){
 
-    let returnedResponse = JSON.parse(response.body);
+function setButtonDisabled() {
+    document.getElementById("fire").disabled = document.getElementById("input").value != "" ? false : true;
     
-    if(returnedResponse.userName ==  document.getElementById('username').value){
-        document.getElementById('responsArea').innerHTML +=  "<br> <p class='current-user'>" + returnedResponse.userName +": "+ returnedResponse.message + "</p>";
-    }
-    else{
-        document.getElementById('responsArea').innerHTML +=  "<br> <p>" + returnedResponse.userName +": "+ returnedResponse.message + "</p>";
-    }
-}
-
-
 }
